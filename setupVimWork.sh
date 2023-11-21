13168158410b3069e68c1347acdc2a8d3521ecba
Dysseboy <33975442+Dysseboy@users.noreply.github.com>
2023-11-21 11:18:06 -0600

    Add setup  script

---
+++ setupVimWork.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get install vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+scp ./vimrc ~/.vim/vimrc
+
+#Install plugs
+vim -c 'PlugInstall' --sync +qa
+
+cd ~/.vim/plugged/YouCompleteMe
+python install.py --all --verbose 
+
+echo "Vim is done"
